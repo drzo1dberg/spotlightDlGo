d2ad95a89094703720f858cbe10737a3c85c3581
michael@Grothe-IT <mnj@grothe.it>
2025-10-31 11:05:11 +0100

    refactored and adds a github workflow to build artifacts

--- main.go
+++ main.go
@@ -21,11 +21,26 @@ import (
 
 // description :
 // A minimal Windows Spotlight wallpaper downloader rewritten in Go.
-
 const (
-	defaultAPI = "https://fd.api.iris.microsoft.com/v4/api/selection?&placement=88000820&bcnt=4&country=%s&locale=%s&fmt=json"
-	userAgent  = "spotlightdl-go/1.0"
+	userAgent = "spotlightdl-go/1.0"
 )
+
+func buildAPIURL(country, locale string) (string, error) {
+	u, err := url.Parse("https://fd.api.iris.microsoft.com/v4/api/selection")
+	if err != nil {
+		return "", nil
+	}
+	q := url.Values{
+		"placement": {"88000820"},
+		"bcnt":      {"4"},
+		"country":   {country},
+		"locale":    {locale},
+		"fmt":       {"json"},
+	}
+	u.RawQuery = q.Encode()
+	return u.String(), nil
+
+}
 
 type (
 	root struct {
@@ -63,7 +78,7 @@ func fetchOnce(client *http.Client, country, locale string) ([]spotImage, error)
 	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
 	defer cancel()
 
-	reqURL := fmt.Sprintf(defaultAPI, country, locale)
+	reqURL, err := buildAPIURL(country, locale)
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, reqURL, nil)
 	if err != nil {
 		return nil, err
@@ -282,6 +297,7 @@ func main() {
 
 		if newInRound == 0 {
 			emptyRounds++
+			time.Sleep(500 * time.Millisecond)
 		} else {
 			emptyRounds = 0
 		}
@@ -291,4 +307,3 @@ func main() {
 		fmt.Printf("done. new=%d\n", totalNew)
 	}
 }
-
